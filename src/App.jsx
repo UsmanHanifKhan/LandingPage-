fd3fc813496ea7f8587492fa7b0a246e806f3e42
Usman khan <123990015+UsmanHanifKhan@users.noreply.github.com>
2024-07-22 17:33:28 +0500

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header"
 import ContactUs from "./pages/ContactUs"
-import Home from "./pages/Home"
+// import Home from "./pages/Home"
 import Section1 from "./pages/Section1"
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
   <>
     <Header />
-    <Home/>
+    {/* <Home/> */}
     <Section1 />
     <ContactUs/>
   </>
